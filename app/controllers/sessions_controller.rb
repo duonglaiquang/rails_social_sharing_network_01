05185a6a59b8c45427fa99e0785fd19a471cfdd6
lampv <lampvhy@gmail.com>
2018-07-24 12:12:28 +0700

    sign up n log in n activate

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by email: params[:session][:email].downcase
+    if @user&.authenticate params[:session][:password]
+      if @user.activated?
+        log_in @user
+        redirect_to @user
+      else
+        flash[:warning] = t ".account_not_activated"
+        redirect_to root_url
+      end
+    else
+      flash.now[:danger] = t "error"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  attr_reader :activation_token
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  before_save :email_downcase
+  before_create :create_activation_digest
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :actions, as: :target
@@ -9,4 +15,52 @@ class User < ApplicationRecord
            foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  validates :name,  presence: true, length: {maximum: Settings.max_name}
+  validates :email, presence: true, length: {maximum: Settings.max_email},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.min_password}
+
+  has_secure_password
+
+  class << self
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def activate
+    update_attributes activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+  def email_downcase
+    email.downcase!
+  end
+
+  def create_activation_digest
+    @activation_token = User.new_token
+    self.activation_digest = User.digest activation_token
+  end
 end
